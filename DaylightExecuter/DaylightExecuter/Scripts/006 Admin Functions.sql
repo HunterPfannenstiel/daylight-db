204ecc57dbec0afc95eca632d5f7d4aa13201fba
ohShews <pfannenstielhunter@gmail.com>
2023-05-27 23:01:59 -0500

    Created admin function to view selections for an item

--- DaylightExecuter/DaylightExecuter/Scripts/006 Admin Functions.sql
+++ DaylightExecuter/DaylightExecuter/Scripts/006 Admin Functions.sql
@@ -25,4 +25,42 @@ BEGIN
 END;
 $func$;
 
+CREATE OR REPLACE FUNCTION store.fetch_item_selections(item_id SMALLINT)
+RETURNS TABLE (name TEXT, price NUMERIC(4,2), description TEXT, image_id INTEGER, initial_group_id SMALLINT, initial_extra_groupings JSON[], initial_item_categories JSON[], initial_weekdays JSON[], initial_range DATERANGE)
+LANGUAGE plpgsql
+AS
+$func$
+BEGIN
+	RETURN QUERY
+	SELECT MI.name, MI.price, MI.description, MI.image_id,
+	G.grouping_id AS initial_group_id, array_agg(json_build_object(EC.name, IEG.extra_group_id)) AS initial_extra_groupings,
+	(SELECT array_agg(tb) FROM store.fetch_item_initial_categories(item_id) tb) AS initial_item_categories, 
+	array_agg(CASE WHEN WA.weekday_id IS NOT NULL THEN json_build_object(WA.weekday_id, true) END) AS initial_weekdays, MI.availability_range AS initial_range
+	FROM store.menu_item MI
+	JOIN store.grouping G ON G.grouping_id = MI.grouping_id
+	LEFT JOIN store.item_extra_group IEG ON IEG.menu_item_id = MI.menu_item_id
+	LEFT JOIN store.extra_group EG ON EG.extra_group_id = IEG.extra_group_id
+	LEFT JOIN store.extra_category EC ON EC.extra_category_id = EG.extra_category_id
+	LEFT JOIN store.weekday_availability WA ON WA.menu_item_id = MI.menu_item_id
+	WHERE MI.menu_item_id = item_id
+	GROUP BY MI.name, MI.price, MI.description, MI.image_id, G.grouping_id, MI.availability_range;
+END;
+$func$;
 
+CREATE OR REPLACE FUNCTION store.fetch_item_initial_categories(item_id SMALLINT)
+RETURNS TABLE (categories JSON)
+LANGUAGE plpgsql
+AS
+$func$
+BEGIN
+	RETURN QUERY
+	SELECT json_build_object(MIC.item_category_id, array_agg(CASE WHEN "IS".item_subcategory_id IS NOT NULL THEN json_build_object("IS".item_subcategory_id, true) END)) AS categories
+	FROM store.menu_item_category MIC
+	LEFT JOIN store.item_category IC ON IC.item_category_id = MIC.item_category_id
+	LEFT JOIN store.menu_item_subcategory MIS ON MIS.menu_item_id = MIC.menu_item_id
+	LEFT JOIN store.item_subcategory "IS" ON "IS".item_category_id = IC.item_category_id 
+		AND "IS".item_subcategory_id = MIS.item_subcategory_id
+	WHERE MIC.menu_item_id = item_id
+	GROUP BY MIC.item_category_id;
+END;
+$func$;
