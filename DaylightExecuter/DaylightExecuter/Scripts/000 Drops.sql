5bc9704d9fc498f12cbab281a2ead5b3383af0d9
Payton Pfannenstiel <payton02@ksu.edu>
2023-05-31 17:42:16 -0500

    Added a procedure for editing user info

--- DaylightExecuter/DaylightExecuter/Scripts/000 Drops.sql
+++ DaylightExecuter/DaylightExecuter/Scripts/000 Drops.sql
@@ -69,4 +69,5 @@ DROP PROCEDURE IF EXISTS store.create_account;
 DROP PROCEDURE IF EXISTS store.modify_menu_item;
 DROP PROCEDURE IF EXISTS store.update_item_extras;
 DROP PROCEDURE IF EXISTS store.create_menu_item;
+DROP PROCEDURE IF EXISTS store.add_user_info;
 DROP TYPE IF EXISTS store.images;
---
+++ DaylightExecuter/DaylightExecuter/Scripts/007 Account Procedures.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE PROCEDURE store.add_user_info(
+	info_account_id INTEGER, 
+	info_first_name TEXT DEFAULT NULL, 
+	info_last_name TEXT DEFAULT NULL, 
+	info_phone_number TEXT DEFAULT NULL, 
+	favorite BOOL DEFAULT false, 
+	info_id INTEGER DEFAULT NULL, 
+	should_update BOOL DEFAULT false
+)
+LANGUAGE plpgsql
+SECURITY DEFINER AS
+$$
+DECLARE favorited_id INTEGER;
+BEGIN
+	IF favorite THEN
+			SELECT UI.user_info_id INTO favorited_id
+			FROM store.user_info UI 
+			WHERE UI.account_id = info_account_id 
+				AND UI.is_favorited = true;
+
+			IF favorited_id IS NOT NULL THEN
+				UPDATE store.user_info
+				SET is_favorited = false
+				FROM store.user_info UI
+				WHERE UI.user_info_id = favorited_id;
+			END IF;
+		END IF;
+		
+	IF info_id IS NOT NULL THEN
+		IF NOT EXISTS (SELECT * FROM store.user_info UI WHERE UI.user_info_id = info_id AND UI.account_id = info_account_id) THEN
+			RAISE EXCEPTION 'User info does not exist for the info_id passed in or the passed in info_account_id is not associated with info_id';
+		END IF;
+		
+		IF should_update THEN
+			UPDATE store.user_info 
+			SET first_name = COALESCE(info_first_name, first_name),
+				last_name = COALESCE(info_last_name, last_name),
+				phone_number = COALESCE(info_phone_number, phone_number),
+				is_favorited = COALESCE(favorite, is_favorited)
+			WHERE user_info_id = info_id;
+		ELSE
+			DELETE FROM store.user_info
+			WHERE user_info_id = info_id;
+		END IF;	
+	ELSE
+		INSERT INTO store.user_info(account_id, first_name, last_name, phone_number, is_favorited)
+		VALUES(info_account_id, info_first_name, info_last_name, info_phone_number, favorite);
+	END IF;
+END;
+$$;
