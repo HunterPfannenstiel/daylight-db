c759f6de1037650743fe2ebc7c584a7b97182fcd
Payton Pfannenstiel <payton02@ksu.edu>
2023-07-30 20:28:32 -0500

    Fixed user info bug; started fixing analytics bug

--- DaylightExecuter/DaylightExecuter/Scripts/050 Analytical Functions.sql
+++ DaylightExecuter/DaylightExecuter/Scripts/050 Analytical Functions.sql
@@ -113,7 +113,7 @@ BEGIN
 				INNER JOIN store.cart_item CI ON C.cart_id = CI.cart_id
 				INNER JOIN store.menu_item_category MIC ON CI.menu_item_id = MIC.menu_item_id
 				INNER JOIN store.item_category IC ON MIC.item_category_id = IC.item_category_id
-					AND IC.name = COALESCE(item_category, IC.name)
+					AND IC.name = item_category
 				RIGHT JOIN store.get_orders_in_date_range(begin_date, end_date, preserve_null_dates) O
 					ON C.cart_id = O.cart_id
 			GROUP BY O.created_on
@@ -127,8 +127,8 @@ BEGIN
 				INNER JOIN store.cart_item CI ON C.cart_id = CI.cart_id
 				INNER JOIN store.menu_item MI ON CI.menu_item_id = MI.menu_item_id
 					AND MI.name = COALESCE(item_name, MI.name)
-				RIGHT JOIN store.get_orders_in_date_range(begin_date, end_date, preserve_null_dates) O
-					ON C.cart_id = O.cart_id
+				INNER JOIN store.get_orders_in_date_range(begin_date, end_date, preserve_null_dates) O
+					ON C.cart_id = O.cart_id OR (preserve_null_dates AND O.cart_id IS NULL)
 			GROUP BY O.created_on
 			ORDER BY created_on;
 	END IF;
@@ -152,8 +152,6 @@ BEGIN
 			FROM store.order O
 				RIGHT JOIN store.get_dates(begin_date, end_date, 'day', '1 day') D 
 					ON DATE_TRUNC('day', O.created_on) = D.date
-			GROUP BY D.date,
-				O.cart_id
 			ORDER BY D.date;
 	ELSE
 		RETURN QUERY
@@ -161,8 +159,6 @@ BEGIN
 				O.cart_id
 			FROM store.order O
 			WHERE O.created_on BETWEEN begin_date AND end_date
-			GROUP BY O.created_on::DATE,
-				O.cart_id
 			ORDER BY O.created_on::DATE;
 	END IF;
 END;
@@ -252,10 +248,11 @@ SELECT MI.name FROM store.menu_item MI INNER JOIN store.menu_item_category MIC O
 	INNER JOIN store.item_category IC ON MIC.item_category_id = IC.item_category_id WHERE IC.name = 'Donuts'
 
 SELECT * FROM store.get_item_analytics('2023-06-15', '2023-07-15', 'weeky', true, NULL, NULL)
-
-SELECT * FROM store.filter_items_and_date_range('2023-06-15', '2023-07-15', true, NULL, NULL)
+select * from store.item_category
+SELECT * FROM store.filter_items_and_date_range('2023-06-1', '2023-07-1', false, NULL, 'Glaze')
+select * from store.cart C join store.cart_item CI on C.cart_id = CI.cart_id join store.menu_item MI on CI.menu_item_id = MI.menu_item_id AND MI.name = 'Glaze' where C.cart_id = 1
 SELECT * FROM store.filter_items_and_date_range('2023-06-15', '2023-07-15', true, 'Featured', NULL)
-SELECT * FROM store.get_orders_in_date_range('2023-06-15', '2023-07-15', false);
+SELECT * FROM store.get_orders_in_date_range('2023-06-1', '2023-07-1', false);
 SELECT * FROM store.get_dates('2023-06-15', '2023-07-15', 'day', '1 day');
 SELECT * FROM store.get_donuts_sold('Glaze');
 SELECT * FROM store.get_monthly_donuts_sold('2023-06-15', '2023-07-15', NULL)
